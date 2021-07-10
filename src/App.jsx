034b76d91d35fe2e48b9200d57541a55ba73988c
I Made Wira <wirasatya19@gmail.com>
2021-07-10 17:15:37 +0800

    first configuration before start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import "./app.scss";
+import Home from "./pages/Home/Home";
+import Topbar from "./components/Topbar/Topbar";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div className="app">
+      <Topbar></Topbar>
+      <Home></Home>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.scss";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <h1>topbar</h1>
+    </div>
+  );
+};
+
+export default Topbar;
